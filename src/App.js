a9e6738daf902357643b9c1e7a6c30c6c459a80b
Sara Girard <sarag.bsa@gmail.com>
2023-05-16 14:12:22 -0400

    component changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React, { useState } from "react";
 import { Grid } from "@mui/material";
 import { Container } from "@mui/system";
+import React, { useState } from "react";
 import Navbar from "./Components/Navbar";
 import Result from "./Components/Result";
 import SliderSelect from "./Components/SliderSelect";
 import TenureSelect from "./Components/TenureSelect";
 
 function App() {
+  const [data, setData] = useState({
+    homeValue: 3000,
+    downPayment: 3000 * 0.2,
+    loanAmount: 3000 * 0.8,
+    loanTerm: 5,
+    interestRate: 5,
+    monthlyPayment: 0,
+  });
+
+  console.log(data)
+
   return (
     <div className="App">
       <Navbar />
       <Container maxWidth="xl" sx={{marginTop:4}}>
         <Grid container spacing={5} alignItems="center">
           <Grid item xs={12} md={6}>
-            <SliderSelect />
-            <TenureSelect />
+            <SliderSelect data={data} setData={setData} />
+            <TenureSelect data={data} setData={setData}/>
           </Grid>
           <Grid item xs={12} md={6}>
-            <Result/>
+            <Result data={data}/>
           </Grid>
         </Grid>
       </Container>
--- src/Components/Common/SliderComponent.js
+++ src/Components/Common/SliderComponent.js
@@ -15,10 +15,35 @@ const SliderComponent = ({
     steps
   }) => {
     return (
-      <Stack my={1.4}>
-  
-      </Stack>
+      
+    <Stack my={1.4}>
+    <Stack gap={1}>
+      <Typography variant="subtitle2">{label}</Typography>
+      <Typography variant="h5">
+        {unit} {amount}
+      </Typography>
+    </Stack>
+    <Slider
+      min={min}
+      max={max}
+      defaultValue={defaultValue}
+      aria-label="Default"
+      valueLabelDisplay="auto"
+      onChange={onChange}
+      value={value}
+      marks
+      step={steps}
+    />
+    <Stack direction="row" justifyContent="space-between">
+      <Typography variant="caption" color="text.secondary">
+        {unit} {min}
+      </Typography>
+      <Typography variant="caption" color="text.secondary">
+        {unit} {max}
+      </Typography>
+    </Stack>
+  </Stack>
     )
   }
   
-  export default SliderComponent
+  export default SliderComponent;
