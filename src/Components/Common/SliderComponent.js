fa9cc940cd633ef07dc07c1de800b1debafe3800
Sara Girard <sarag.bsa@gmail.com>
2023-05-16 13:03:46 -0400

    first commit

---
+++ src/Components/Common/SliderComponent.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Slider from "@mui/material/Slider";
+import { Typography } from "@mui/material";
+import { Stack } from "@mui/system";
+
+const SliderComponent = ({
+    defaultValue,
+    min,
+    max,
+    label,
+    unit,
+    onChange,
+    amount,
+    value,
+    steps
+  }) => {
+    return (
+      <Stack my={1.4}>
+  
+      </Stack>
+    )
+  }
+  
+  export default SliderComponent
--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>
+    <CssBaseline />
+  </ThemeProvider>
+</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
